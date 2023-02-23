dfa2c0cb287e455e80952a1fc9aa88c806541566
Haidar <16521522@mahasiswa.itb.ac.id>
2023-02-23 22:36:11 +0700

    Adding sort

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from point import point
+from point import point as p
 # Develop
 n = int(input("Masukkan nilai N!\n"))
-ArrayOfPoint = point.randomPoint(n)
+ArrayOfPoint = p.sortPoint(p.randomPoint(n))
--- src/point.py
+++ src/point.py
@@ -8,8 +8,8 @@ class point:
 
     def randomPoint(numberOfPoint):
         ArrayOfpoint = []
-        temp = point(0,0,0)
         for i in range(numberOfPoint):
+            temp = point(0,0,0)
             random_number = random.randint(0, 1000)
             temp.X = random_number
             random_number = random.randint(0, 1000)
@@ -18,6 +18,9 @@ class point:
             temp.Z = random_number
             ArrayOfpoint.append(temp)
         return ArrayOfpoint
+    
+    def sortPoint(arrayOfP):
+        return sorted(arrayOfP, key=lambda x: x.X)
     
     def __str__(self):
         return f"({self.X},{self.Y},{self.Z})"
